0c294f959898c8757261655dbad5bb2ab753e793
Karl Horning <32777806+Karl-Horning@users.noreply.github.com>
2024-01-21 18:47:18 +0000

    Added helper to format dates

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -14,4 +14,18 @@ const getByAuthorIdWhere = (authorId) => ({
     },
 });
 
-module.exports = { getByIdWhere, getByAuthorIdWhere };
+/**
+ * Converts a timestamp to the YYYY-MM-DD format.
+ * @param {number} timestamp - The timestamp to convert.
+ * @returns {string} The formatted date in YYYY-MM-DD format.
+ */
+const formatTimestampToYYYYMMDD = (timestamp) => {
+    const date = new Date(timestamp);
+    const year = date.getUTCFullYear();
+    const month = (date.getUTCMonth() + 1).toString().padStart(2, "0");
+    const day = date.getUTCDate().toString().padStart(2, "0");
+
+    return `${year}-${month}-${day}`;
+};
+
+module.exports = { getByIdWhere, getByAuthorIdWhere, formatTimestampToYYYYMMDD };
